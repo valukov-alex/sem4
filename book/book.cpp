5fd8edabfad872c2ec889e33e5232821545f38f3
Vlad <vlad.miryaha@gmail.com>
2017-02-23 22:51:19 +0300

    fixit
    read comments

--- book/book.cpp
+++ book/book.cpp
@@ -10,6 +10,9 @@ std::string prepare(std::string& s)
 	std::string res;
 	std::transform(s.begin(), s.end(), s.begin(), ::tolower);
 	res = s;
+	
+	// FIXIT: в рассылке же писал, что так не нужно проверять
+	// нужно либо создать множество из символов пунктуации, либо воспользоваться ф-ями ispunct или isalpha
 	if ( (res[res.length()] == ',') || (res[res.length()] == '.') || (res[res.length()] == ';') )
 		res.pop_back();
 
@@ -21,7 +24,8 @@ struct Statistic {
 	std::string word;
 };
 
-
+// FIXIT: по константной ссылке нужно передать, чтобы избежать ненужного копирования
+// обычно в подобных ф-ях аргументы называют lhs и rhs: left/right hand side
 bool comp(Statistic p1, Statistic p2)
 {
 	return p1.count > p2.count;
@@ -47,11 +51,14 @@ int main()
 		int i = 0;
 		for (it = mymap.begin(); it != mymap.end(); ++it) {
 			s.push_back(stat);
+			// it->second
+			// it->first
 			s.at(i).count = (*it).second;
 			s.at(i).word = (*it).first;
 			i++;
 		}
 		std::sort(s.begin(), s.end(), comp);
+		// FIXIT: для 50 отдельная константа
 		for ( i = 0; i < 50; i++) {
 			std::cout << s[i].count << ' ' << s[i].word << std::endl;
 		}
